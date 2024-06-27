4963e18595d3be5a0af94dd555f6e6a20c9f0183
Marcus0086 <guptamarcus42@gmail.com>
2024-06-27 12:39:31 +0530

    feat: Improve error handling in PdfDownloadMiddleware

--- airbyte-integrations/connectors/source-website-scraper/source_website_scraper/middleware/pdf.py
+++ airbyte-integrations/connectors/source-website-scraper/source_website_scraper/middleware/pdf.py
@@ -1,11 +1,18 @@
 import fitz
+from airbyte_cdk.logger import init_logger
 
+logger = init_logger("airbyte")
 
 class PdfDownloadMiddleware:
 
     def process_response(self, response):
-        pdf_document = fitz.open(stream=response.body, filetype="pdf")
-        text = ""
-        for page in pdf_document:
-            text += page.get_text()
-        return text
+        try:
+            pdf_document = fitz.open(stream=response.body, filetype="pdf")
+            text = ""
+            for page in pdf_document:
+                text += page.get_text()
+            return text
+        except Exception as e:
+            # Log the exception if needed
+            logger.error(f"Error while processing pdf: {e}")
+            return ""
